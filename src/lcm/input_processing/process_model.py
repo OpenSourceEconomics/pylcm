5dfd149e19df396c20f8b42979f472dd3c8e0b80
Tim Mensinger <mensingertim@gmail.com>
2025-03-24 14:09:27 +0100

    Ignore new mypy errors

--- src/lcm/input_processing/process_model.py
+++ src/lcm/input_processing/process_model.py
@@ -210,4 +210,4 @@ def _get_stochastic_weight_function(
         args = all_as_args(args, kwargs, arg_names=new_kwargs)
         return params["shocks"][name][*args]
 
-    return weight_func
+    return cast(InternalUserFunction, weight_func)
