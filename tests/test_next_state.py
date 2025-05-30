390ae7c84faa5edad8fe91ab689f1d0799826ab1
Tim Mensinger <mensingertim@gmail.com>
2025-05-30 17:04:55 +0200

    Fix some tests / use remote dags

--- tests/test_next_state.py
+++ tests/test_next_state.py
@@ -8,7 +8,7 @@ from pybaum import tree_equal
 from lcm.input_processing import process_model
 from lcm.interfaces import InternalModel
 from lcm.next_state import _create_stochastic_next_func, get_next_state_function
-from lcm.typing import ParamsDict, Scalar, ShockType, Target
+from lcm.typing import ContinuousState, ParamsDict, ShockType, Target
 from tests.test_models import get_model_config
 
 
@@ -34,13 +34,13 @@ def test_get_next_state_function_with_solve_target():
 
 
 def test_get_next_state_function_with_simulate_target():
-    def f_a(state: Array, params: ParamsDict) -> Scalar:  # noqa: ARG001
+    def f_a(state: ContinuousState, params: ParamsDict) -> Array:  # noqa: ARG001
         return state[0]
 
-    def f_b(state: Scalar, params: ParamsDict) -> Scalar:  # noqa: ARG001
+    def f_b(state: ContinuousState, params: ParamsDict) -> Array:  # noqa: ARG001
         return None  # type: ignore[return-value]
 
-    def f_weight_b(state: Scalar, params: ParamsDict) -> Array:  # noqa: ARG001
+    def f_weight_b(state: ContinuousState, params: ParamsDict) -> Array:  # noqa: ARG001
         return jnp.array([[0.0, 1.0]])
 
     functions = {
