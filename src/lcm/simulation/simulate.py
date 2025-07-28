b24bf511c2b5fb0855e133b006bffe65d179d69a
mj023 <max.jahn45@gmail.com>
2025-07-28 17:11:41 +0200

    TEST commit

--- src/lcm/simulation/simulate.py
+++ src/lcm/simulation/simulate.py
@@ -47,7 +47,7 @@ def solve_and_simulate(
 ) -> pd.DataFrame:
     """First solve the model and then simulate the model forward in time.
 
-    Same docstring as `simulate` mutatis mutandis.
+    Same docstring as `simulate` mutatis mutandis. TEST
 
     """
     V_arr_dict = solve_model(params)
