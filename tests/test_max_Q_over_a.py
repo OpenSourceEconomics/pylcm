41c34a9d0d1c6a9150f3d18255ffd2f603f84b16
Tim Mensinger <mensingertim@gmail.com>
2025-08-18 15:55:41 +0200

    Improve readability of max_Q_over_a test module

--- tests/test_max_Q_over_a.py
+++ tests/test_max_Q_over_a.py
@@ -1,14 +1,13 @@
 from __future__ import annotations
 
-from functools import partial
 from typing import TYPE_CHECKING
 
 import jax.numpy as jnp
 import pytest
-from jax import Array, vmap
+from jax import Array
 from numpy.testing import assert_array_equal
 
-from lcm.dispatchers import simulation_spacemap, vmap_1d
+from lcm.dispatchers import simulation_spacemap
 from lcm.input_processing import process_model
 from lcm.interfaces import StateActionSpace, Target
 from lcm.max_Q_over_a import get_argmax_and_max_Q_over_a, get_max_Q_over_a
@@ -16,6 +15,11 @@ from lcm.max_Q_over_c import get_argmax_and_max_Q_over_c, get_max_Q_over_c
 from lcm.max_Qc_over_d import get_argmax_and_max_Qc_over_d, get_max_Qc_over_d
 from lcm.next_state import get_next_state_function
 from lcm.Q_and_F import get_Q_and_F
+from lcm.simulation.simulate import (
+    _lookup_actions_from_indices,
+    _lookup_optimal_continuous_actions,
+    _lookup_values_from_indices,
+)
 from lcm.state_action_space import create_state_action_space, create_state_space_info
 from tests.test_models import (
     get_model_config,
@@ -29,10 +33,11 @@ if TYPE_CHECKING:
 
 @pytest.fixture
 def model_input():
-    model_config = get_model_config("iskhakov_et_al_2017_stripped_down", n_periods=1)
-    actions = model_config.actions
+    _model_config = get_model_config("iskhakov_et_al_2017_stripped_down", n_periods=1)
+    # Modify the model to have a coarser continuous action space for testing
+    actions = _model_config.actions
     actions["consumption"] = actions["consumption"].replace(stop=20)  # type: ignore[attr-defined]
-    model_config = model_config.replace(actions=actions)
+    model_config = _model_config.replace(actions=actions)
     model = process_model(model_config)
 
     state_space_info = create_state_space_info(
@@ -60,6 +65,14 @@ def model_input():
 
 
 def test_max_Q_over_a_equal(model_input):
+    """Test max_Q_over_a is equivalent to max_Qc_over_d (max_Q_over_c).
+
+    In this test we check that taking the maximum of Q over all actions
+    (max_Q_over_a) is equivalent to taking the maximum of Q over continuous actions
+    (max_Q_over_c), conditional on the discrete actions, and then over discrete actions
+    (max_Qc_over_d); since these operations should be mathematically equivalent.
+
+    """
     params = model_input["params"]
     state_space_info = model_input["state_space_info"]
     state_action_space = model_input["state_action_space"]
@@ -70,6 +83,11 @@ def test_max_Q_over_a_equal(model_input):
         next_state_space_info=state_space_info,
         period=0,
     )
+    next_V_arr = jnp.zeros((2, 2))
+
+    # ----------------------------------------------------------------------------------
+    # Maximum over all actions directly
+    # ----------------------------------------------------------------------------------
     max_Q_over_a = get_max_Q_over_a(
         Q_and_F=Q_and_F,
         actions_names=(
@@ -79,6 +97,17 @@ def test_max_Q_over_a_equal(model_input):
         states_names=(*state_action_space.states,),
     )
 
+    V_arr_a = max_Q_over_a(
+        **state_action_space.states,
+        **state_action_space.discrete_actions,
+        **state_action_space.continuous_actions,
+        next_V_arr=next_V_arr,
+        params=params,
+    )
+
+    # ----------------------------------------------------------------------------------
+    # Maximum over continuous actions and then over discrete actions
+    # ----------------------------------------------------------------------------------
     max_Q_over_c = get_max_Q_over_c(
         Q_and_F=Q_and_F,
         continuous_actions_names=(*state_action_space.continuous_actions,),
@@ -92,38 +121,55 @@ def test_max_Q_over_a_equal(model_input):
         variable_info=model.variable_info,
         is_last_period=False,
     )
-
-    V_arr_a = max_Q_over_a(
-        **state_action_space.states,
-        **state_action_space.discrete_actions,
-        **state_action_space.continuous_actions,
-        next_V_arr=jnp.zeros((2, 2)),
-        params=params,
-    )
-
     Qc_arr = max_Q_over_c(
         **state_action_space.states,
         **state_action_space.discrete_actions,
         **state_action_space.continuous_actions,
-        next_V_arr=jnp.zeros((2, 2)),
+        next_V_arr=next_V_arr,
         params=params,
     )
     V_arr_c_d = max_Qc_over_d(Qc_arr, params=params)
 
+    # ----------------------------------------------------------------------------------
+    # Assert equality
+    # ----------------------------------------------------------------------------------
     assert_array_equal(V_arr_a, V_arr_c_d)
 
 
 def test_argmax_Q_over_a_equal(model_input):
+    """Test argmax_Q_over_a is equivalent to argmax_Qc_over_d (argmax_Q_over_c).
+
+    In this test we check that taking the argmax of Q over all actions
+    (argmax_Q_over_a) is equivalent to taking the argmax of Q over continuous actions
+    (argmax_Q_over_c), conditional on the discrete actions, and then over discrete
+    actions (argmax_Qc_over_d); since these operations should be mathematically
+    equivalent.
+
+    """
     params = model_input["params"]
     state_space_info = model_input["state_space_info"]
     state_action_space = model_input["state_action_space"]
     model = model_input["model"]
+
     Q_and_F = get_Q_and_F(
         model=model,
         next_state_space_info=state_space_info,
         period=0,
     )
-    argmax_and_max_Q_over_a_func = get_argmax_and_max_Q_over_a(
+    next_V_arr = jnp.zeros((2, 2))
+
+    discrete_actions_grid_shape = tuple(
+        len(grid) for grid in state_action_space.discrete_actions.values()
+    )
+    continuous_actions_grid_shape = tuple(
+        len(grid) for grid in state_action_space.continuous_actions.values()
+    )
+    actions_grid_shape = discrete_actions_grid_shape + continuous_actions_grid_shape
+
+    # ----------------------------------------------------------------------------------
+    # Argmax over all actions directly
+    # ----------------------------------------------------------------------------------
+    _argmax_and_max_Q_over_a_func = get_argmax_and_max_Q_over_a(
         Q_and_F=Q_and_F,
         actions_names=(
             *state_action_space.discrete_actions,
@@ -131,29 +177,37 @@ def test_argmax_Q_over_a_equal(model_input):
         ),
     )
     argmax_and_max_Q_over_a = simulation_spacemap(
-        argmax_and_max_Q_over_a_func,
+        _argmax_and_max_Q_over_a_func,
         actions_names=(),
         states_names=tuple(state_action_space.states),
     )
-    argmax_and_max_Q_over_c_func = get_argmax_and_max_Q_over_c(
+    indices_optimal_actions, V_arr_a = argmax_and_max_Q_over_a(
+        **state_action_space.states,
+        **state_action_space.discrete_actions,
+        **state_action_space.continuous_actions,
+        next_V_arr=next_V_arr,
+        params=params,
+    )
+    optimal_actions_a = _lookup_actions_from_indices(
+        indices_optimal_actions=indices_optimal_actions,
+        actions_grid_shape=actions_grid_shape,
+        state_action_space=state_action_space,
+    )
+
+    # ----------------------------------------------------------------------------------
+    # Argmax over continuous actions and then over discrete actions
+    # ----------------------------------------------------------------------------------
+    _argmax_and_max_Q_over_c_func = get_argmax_and_max_Q_over_c(
         Q_and_F=Q_and_F,
         continuous_actions_names=(*state_action_space.continuous_actions,),
     )
     argmax_and_max_Q_over_c = simulation_spacemap(
-        argmax_and_max_Q_over_c_func,
+        _argmax_and_max_Q_over_c_func,
         actions_names=tuple(state_action_space.discrete_actions),
         states_names=tuple(state_action_space.states),
     )
     argmax_and_max_Qc_over_d = get_argmax_and_max_Qc_over_d(
         variable_info=model.variable_info,
-    )
-
-    indices_optimal_actions, V_arr = argmax_and_max_Q_over_a(
-        **state_action_space.states,
-        **state_action_space.discrete_actions,
-        **state_action_space.continuous_actions,
-        next_V_arr=jnp.zeros((2, 2)),
-        params=params,
     )
 
     indices_argmax_Q_over_c, Qc_arr = argmax_and_max_Q_over_c(
@@ -163,22 +217,15 @@ def test_argmax_Q_over_a_equal(model_input):
         next_V_arr=jnp.zeros((2, 2)),
         params=params,
     )
-    indices_optimal_discrete_actions, V_arr = argmax_and_max_Qc_over_d(
+    indices_optimal_discrete_actions, V_arr_c_d = argmax_and_max_Qc_over_d(
         Qc_arr, params=params
     )
-    discrete_actions_grid_shape = tuple(
-        len(grid) for grid in state_action_space.discrete_actions.values()
-    )
-    continuous_actions_grid_shape = tuple(
-        len(grid) for grid in state_action_space.continuous_actions.values()
-    )
-    actions_grid_shape = discrete_actions_grid_shape + continuous_actions_grid_shape
     indices_optimal_continuous_actions = _lookup_optimal_continuous_actions(
         indices_argmax_Q_over_c=indices_argmax_Q_over_c,
         discrete_argmax=indices_optimal_discrete_actions,
         discrete_actions_grid_shape=discrete_actions_grid_shape,
     )
-    optimal_actions_c_d = _lookup_actions_from_indices(
+    optimal_actions_c_d = _lookup_actions_from_indices_c_d(
         indices_optimal_discrete_actions=indices_optimal_discrete_actions,
         indices_optimal_continuous_actions=indices_optimal_continuous_actions,
         discrete_actions_grid_shape=discrete_actions_grid_shape,
@@ -186,50 +233,26 @@ def test_argmax_Q_over_a_equal(model_input):
         state_action_space=state_action_space,
     )
 
-    optimal_actions_a = _lookup_actions_from_indices_2(
-        indices_optimal_actions=indices_optimal_actions,
-        actions_grid_shape=actions_grid_shape,
-        state_action_space=state_action_space,
-    )
-
+    # ----------------------------------------------------------------------------------
+    # Assert equality
+    # ----------------------------------------------------------------------------------
     assert_array_equal(
         optimal_actions_a["retirement"], optimal_actions_c_d["retirement"]
     )
     assert_array_equal(
         optimal_actions_a["consumption"], optimal_actions_c_d["consumption"]
     )
+    assert_array_equal(V_arr_a, V_arr_c_d)
 
 
-@partial(vmap_1d, variables=("indices_argmax_Q_over_c", "discrete_argmax"))
-def _lookup_optimal_continuous_actions(
-    indices_argmax_Q_over_c: IntND,
-    discrete_argmax: IntND,
-    discrete_actions_grid_shape: tuple[int, ...],
-) -> IntND:
-    """Look up the optimal continuous action index given index of discrete action.
-
-    Args:
-        indices_argmax_Q_over_c: Index array of optimal continous actions conditional on
-            discrete actions and states.
-        discrete_argmax: Index array of optimal discrete actions.
-        discrete_actions_grid_shape: Shape of the discrete actions grid.
-
-    Returns:
-        Index array of optimal continuous actions.
-
-    """
-    indices = jnp.unravel_index(discrete_argmax, shape=discrete_actions_grid_shape)
-    return indices_argmax_Q_over_c[indices]
-
-
-def _lookup_actions_from_indices(
+def _lookup_actions_from_indices_c_d(
     indices_optimal_discrete_actions: IntND,
     indices_optimal_continuous_actions: IntND,
     discrete_actions_grid_shape: tuple[int, ...],
     continuous_actions_grid_shape: tuple[int, ...],
     state_action_space: StateActionSpace,
 ) -> dict[str, Array]:
-    """Lookup optimal actions from indices.
+    """Lookup optimal actions from indices of discrete and continuous actions.
 
     Args:
         indices_optimal_discrete_actions: Indices of optimal discrete actions.
@@ -255,55 +278,3 @@ def _lookup_actions_from_indices(
     )
 
     return optimal_discrete_actions | optimal_continuous_actions
-
-
-def _lookup_actions_from_indices_2(
-    indices_optimal_actions: IntND,
-    actions_grid_shape: tuple[int, ...],
-    state_action_space: StateActionSpace,
-) -> dict[str, Array]:
-    """Lookup optimal actions from indices.
-
-    Args:
-        indices_optimal_actions: Indices of optimal actions.
-        actions_grid_shape: Shape of the actions grid.
-        state_action_space: StateActionSpace instance.
-
-    Returns:
-        Dictionary of optimal actions.
-
-    """
-    return _lookup_values_from_indices(
-        flat_indices=indices_optimal_actions,
-        grids=state_action_space.discrete_actions
-        | state_action_space.continuous_actions,
-        grids_shapes=actions_grid_shape,
-    )
-
-
-def _lookup_values_from_indices(
-    flat_indices: IntND,
-    grids: dict[str, Array],
-    grids_shapes: tuple[int, ...],
-) -> dict[str, Array]:
-    """Retrieve values from indices.
-
-    Args:
-        flat_indices: General indices. Represents the index of the flattened grid.
-        grids: Dictionary of grid values.
-        grids_shapes: Shape of the grids. Is used to unravel the index.
-
-    Returns:
-        Dictionary of values.
-
-    """
-    nd_indices = vmapped_unravel_index(flat_indices, grids_shapes)
-    return {
-        name: grid[index]
-        for (name, grid), index in zip(grids.items(), nd_indices, strict=True)
-    }
-
-
-# vmap jnp.unravel_index over the first axis of the `indices` argument, while holding
-# the `shape` argument constant (in_axes = (0, None)).
-vmapped_unravel_index = vmap(jnp.unravel_index, in_axes=(0, None))
